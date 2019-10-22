ac71523a739bae23e784388ce538ebfad87e6b4f
Schirrms <schirrms@schirrms.net>
2019-10-22 15:18:38 +0200

    initial version - Only for DBServer for now (0.1.0.0)

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # iTop-Database-Size
 Adds a 'Used Space' field for DB Server and Schemas
+
+The field is stored in Bytes and the display is in human readable format (KiB, MiB, GiB, TiB, PiB)
---
+++ schirrms-database-size/module.schirrms-database-size.php
@@ -0,0 +1,52 @@
+<?php
+//
+// iTop module definition file
+//
+
+SetupWebPage::AddModule(
+	__FILE__, // Path to the current file, all other file names are relative to the directory containing this file
+	'schirrms-database-size/0.1.0',
+	array(
+		// Identification
+		//
+		'label' => 'Add a used space field for DB Server and Instance',
+		'category' => 'business',
+
+		// Setup
+		//
+		'dependencies' => array(
+			'itop-config-mgmt/2.5.0'
+		),
+		'mandatory' => false,
+		'visible' => true,
+
+		// Components
+		//
+		'datamodel' => array(
+			'model.schirrms-database-size.php'
+		),
+		'webservice' => array(
+			
+		),
+		'data.struct' => array(
+			// add your 'structure' definition XML files here,
+		),
+		'data.sample' => array(
+			// add your sample data XML files here,
+		),
+		
+		// Documentation
+		//
+		'doc.manual_setup' => '', // hyperlink to manual setup documentation, if any
+		'doc.more_information' => '', // hyperlink to more information, if any 
+
+		// Default settings
+		//
+		'settings' => array(
+			// Module specific settings go here, if any
+		),
+	)
+);
+
+
+?>
